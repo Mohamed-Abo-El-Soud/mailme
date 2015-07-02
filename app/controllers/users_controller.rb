50e57efdae61cfd0ad1cf4c04e8a956a3f453c4b
MSoud <mohamed.aboelsoud@seagate.com>
2015-07-02 12:13:39 +0000

    no change, for compatibility purposes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
   
-  private 
+  private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
